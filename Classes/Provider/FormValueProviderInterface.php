7e9f65eac4f93da334bf80b5da913cbc8f7fdfc9
Tobias Kretschmann <t.kretschmann@neusta.de>
2022-05-09 16:09:01 +0200

    [BUGFIX] specify return type for getStoredValue in FormValueProviderInterface

--- Classes/Provider/FormValueProviderInterface.php
+++ Classes/Provider/FormValueProviderInterface.php
@@ -5,5 +5,11 @@ namespace Neusta\Modmod\Provider;
 
 interface FormValueProviderInterface
 {
+    /**
+     * @param string $pluginName
+     * @param string $name
+     *
+     * @return mixed
+     */
     public function getStoredValue(string $pluginName, string $name);
 }
